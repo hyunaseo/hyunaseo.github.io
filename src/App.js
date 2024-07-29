c199b1ef2257a7bf216d8df9b41ed1c96d096b8b
Hyuna <hyuna@Hyunaui-MacBookAir.local>
2024-07-29 13:11:50 +0900

    Add heading

--- src/App.js
+++ src/App.js
@@ -5,21 +5,35 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>I'm Hyuna Seo!</h1>
+        {/* 여기에 추가적인 콘텐츠를 작성합니다. */}
       </header>
+      <main>
+        {/* 여기에 메인 콘텐츠를 작성합니다. */}
+      </main>
     </div>
   );
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
