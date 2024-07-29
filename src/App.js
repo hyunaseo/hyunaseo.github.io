4dd247518fbd2c62b4be79d89271acc14f9a297d
Hyuna <hyuna@Hyunaui-MacBookAir.local>
2024-07-29 13:35:25 +0900

    Add title and hyper link

--- src/App.js
+++ src/App.js
@@ -5,35 +5,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>I'm Hyuna Seo</h1>
-        {/* 여기에 추가적인 콘텐츠를 작성합니다. */}
+        <nav>
+          <a href="/" className="site-title">I'm Hyuna Seo!</a>
+        </nav>
       </header>
       <main>
-        {/* 여기에 메인 콘텐츠를 작성합니다. */}
+        <h1>I'm Hyuna Seo!</h1>
+        {/* 여기에 추가적인 콘텐츠를 작성합니다. */}
       </main>
     </div>
   );
 }
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
