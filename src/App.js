134c32a0e667dfc48304b1872f8440bef3988988
Hyuna <hyuna@Hyunaui-MacBookAir.local>
2024-08-20 22:10:10 +0900

    Add news

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ function App() {
               <ul className="publication-list">
                 <li className="publication-item">
                   <div className="publication-video">
-                    <iframe width="320" height="240" src="https://www.youtube.com/embed/_aDg-ykkT-8" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                    <iframe width="320" height="240" src="https://youtu.be/R0vnikegaKo" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
                   </div>
                   <div className="publication-description">
                     <h3>GradualReality: Enhancing Physical Object Interaction in Virtual Reality via Interaction State-Aware Blending</h3>
